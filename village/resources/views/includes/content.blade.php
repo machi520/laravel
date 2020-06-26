ab40f2cf553d199580bc72cd0f58aee73b54a3a8
machi520 <66148552+machi520@users.noreply.github.com>
2020-06-27 02:55:56 +0900

    画像の大きさを修正

--- village/resources/views/includes/content.blade.php
+++ village/resources/views/includes/content.blade.php
@@ -14,16 +14,16 @@
         </p>
         <h3>Q1：どのような立場から政治参加している？</h3>
         <form action="#" name="">
-            <label for=""><input type="radio" name="Q1" id="">国会議員・首長・地方議員</label>
-            <label for=""><input type="radio" name="Q1" id="">官庁職員・地方自治体職員</label>
-            <label for=""><input type="radio" name="Q1" id="">有権者・無権者</label>
+            <label for="A1"><input type="radio" name="Q1" id="A1">国会議員・首長・地方議員</label>
+            <label for="A2"><input type="radio" name="Q1" id="A2">官庁職員・地方自治体職員</label>
+            <label for="A3"><input type="radio" name="Q1" id="A3">有権者・無権者</label>
         </form>
         <div class="discription">
         <p id="D1">日本がもし100人の村なら、この村には○人の議員たち、○人の職員が働いています。
             それ以外の村人は○人が有権者、残りの○人は立候補や投票権を持っていない人たちです。
         </p>
 
-        
+
         </div>
 
     </div>
